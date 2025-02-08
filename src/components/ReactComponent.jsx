bc5bb92c94279e48689957e5e7bb3511e3ed59fe
ParkSeoHai <anhhai282003@gmail.com>
2025-02-08 12:30:10 +0700

    init app

---
+++ src/components/ReactComponent.jsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+   return <h1>Hello, Astro with React!</h1>;
+ };
+
+export default Hello;
---
+++ src/components/VueComponent.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1 class="font-bold mt-20">Hello, Astro with Vue.js!</h1>
+</template>
